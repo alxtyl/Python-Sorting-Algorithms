2cea6a6131250a2d1f84a156882cbe6022875c2b
Alex Tyler <bip760@icloud.com>
2019-01-02 18:42:13 -0800

    changed amount nums are generated

--- generator.py
+++ generator.py
@@ -5,11 +5,11 @@ irand = randrange(0, 1000001)
 f = open('num.txt', 'w')
 x = 0
 
-while x < 100000:
+while x < 10000:
     x += 1
-    irand = randrange(0, 1000001)
+    irand = randrange(0, 10001)
     f.write(str(irand))
     f.write("\n")
-    if (x == 100000):
+    if (x == 10000):
         f.close()
         break
