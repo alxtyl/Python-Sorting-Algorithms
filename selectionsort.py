4724a2a38378129fefe4ef68e432344f6437b00b
Alex <bip760@icloud.com>
2019-01-14 17:09:13 -0800

    Added selection sort

---
+++ selectionsort.py
@@ -0,0 +1,25 @@
+f = open('num.txt', 'r')
+fp = open('selectionsort.txt', 'w')
+
+arr = []
+
+for line in f:
+    arr.append(int(line.strip()))
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        minimum = i
+
+        for j in range(i + 1, len(arr)):
+            # Select the smallest value
+            if arr[j] < arr[minimum]:
+                minimum = j
+
+        # Place it at the front of the
+        # sorted end of the array
+        arr[minimum], arr[i] = arr[i], arr[minimum]
+
+        
+    return arr
+
+fp.write('\n'.join(map(str, selection_sort(arr))))
