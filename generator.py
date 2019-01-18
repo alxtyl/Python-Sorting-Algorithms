d43fcda0c688127e1c2bfe66f332abfb9b9c7984
Alex <bip760@icloud.com>
2019-01-17 16:50:51 -0800

    Started work on main script for comparing times

--- generator.py
+++ generator.py
@@ -2,14 +2,17 @@ from random import randrange
 
 # Opening file and declaring loop counter
 f = open('num.txt', 'w')
-x = 0
 
-# Generating nums
-while x < 1000:
-    x += 1
-    irand = randrange(0, 1001)
-    f.write(str(irand))
-    f.write("\n")
+def generator():
+    x = 0
+    # Generating nums
+    while x < 1000:
+        x += 1
+        irand = randrange(0, 1001)
+        f.write(str(irand))
+        f.write("\n")
+
+generator()
 
 # Closing file object
 f.close()
---
+++ main.py
@@ -0,0 +1,5 @@
+import generator
+import bubblesort
+import mergesort
+import quicksort
+import selectionsort
