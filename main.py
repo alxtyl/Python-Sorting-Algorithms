9a1d66247ecb2c50b29aee88a576211c9f437e27
Alex <bip760@icloud.com>
2019-01-21 18:02:12 -0800

    Cleaned up code in main, still need to fix file writing issue

--- main.py
+++ main.py
@@ -1,55 +1,41 @@
 import time
-import timeit
 from generator import generator
 from bubblesort import bubbleSort
-from mergesort import merge_sort, merge
+from mergesort import merge_sort
 from quicksort import quickSort
 from selectionsort import selectionSort
 
-f = open('num.txt', 'r')
-bubbleWrite = open('bubblesort.txt', 'w')
-mergeWrite = open('mergesort.txt', 'w')
-quicksortWrite = open('quicksort.txt', 'w')
-selectionsortWrite = open('selectionsort.txt', 'w')
+arr = []
 
 generator()
 
-unsorted_list = []
-
-for line in f:
-    unsorted_list.append(int(line.strip()))
+for line in fp:
+   arr.append(int(line.strip()))
 
 start = time.process_time()
-bubbleSort(unsorted_list)
+bubbleSort(arr)
 end = time.process_time()
 elapsed = (end - start) * (10**6)
 print("Time for Bubblesort:")
 print("%.3f" % (elapsed))
 
 start = time.process_time()
-merge_sort(unsorted_list)
+merge_sort(arr)
 end = time.process_time()
 elapsed = (end - start) * (10**6)
 print("Time for Mergesort:")
 print("%.3f" % (elapsed))
-mergeWrite.write('\n'.join(map(str, merge_sort(unsorted_list))))
 
 start = time.process_time()
-quickSort(unsorted_list)
+quickSort(arr)
 end = time.process_time()
 elapsed = (end - start) * (10**6)
 print("Time for Quicksort:")
 print("%.3f" % (elapsed))
 
 start = time.process_time()
-selectionSort(unsorted_list)
+selectionSort(arr)
 end = time.process_time()
 elapsed = (end - start) * (10**6)
 print("Time for Selectionsort:")
 print("%.3f" % (elapsed))
-
-f.close()
-bubbleWrite.close()
-mergeWrite.close()
-quicksortWrite.close()
-selectionsortWrite.close()
